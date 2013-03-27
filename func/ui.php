41a479edd7c1a0eddc880da2d1c442689d8fecc4
Nullivex <contact@nullivex.com>
2013-03-26 21:36:47 -0700

    fixed issue with tpl calls

--- func/ui.php
+++ func/ui.php
@@ -33,20 +33,20 @@ function alert($msg,$success=true,$delayed=false){
 		return true;
 	//add to runtime alerts
 	} else {
-		if(!class_exists('Tpl'))
+		if(!class_exists('\LSS\Tpl'))
 			throw new Exception('Tpl library must be available for alerts');
-		$alert = Tpl::_get()->get('alert');
+		$alert = \LSS\Tpl::_get()->get('alert');
 		if(!is_array($alert)) $alert = array();
 		$alert[$type][]['msg'] = $msg;
-		Tpl::_get()->set('alert',$alert);
+		\LSS\Tpl::_get()->set('alert',$alert);
 		return true;
 	}
 	return false;
 }
 
 function redirect($url){
-	if(empty($url) && class_exists('Url') && Url::_isCallable('home'))
-		$url = Url::home();
+	if(empty($url) && class_exists('\LSS\Url') && \LSS\Url::_isCallable('home'))
+		$url = \LSS\Url::home();
 	elseif(empty($url))
 		$url = '/'; //should get us home
 	header('Location: '.$url);
